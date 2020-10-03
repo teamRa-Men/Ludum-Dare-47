637252cbee66dc45bdc72cd1a63141cb6c2544bd
Manny <teamramenyoobee@gmail.com>
2020-10-03 16:12:37 +1300

    grid

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,34 +4,80 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController instance;
+
     private List<Vector3> gridPositions = new List<Vector3>();
+    public Base playerBase;
+    public List<Robot> robots;
     public List<Floor> floor;
     public List<Wall> walls;
     public List<Enemy> enemies;
     public List<Resource> resources;
+    public float timeStep, time;
 
-    public GameObject floorPrefab, wallPrefab, resourcePrefab, enemyPrefab;
+    public GameObject robotPrefab, floorPrefab, wallPrefab, resourcePrefab, enemyPrefab;
 
-    public Vector2 gridSize = new Vector2(8,8);
+    public Vector2 gridSize = new Vector2(14,10);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null) {
+            instance = this;
+        }
+        InitGrid();
     }
 
-    void initGrid(){
+    void InitGrid(){
         gridPositions.Clear();
+     
         for (int i = 0; i < gridSize.x; i++){
             for(int j = 0; j < gridSize.y; j++){
-                Vector3 pos = new Vector3(i, j, 0);
+                Vector3 pos = new Vector3(i-gridSize.x/2, j-gridSize.y/2, 0);
                 gridPositions.Add(pos);
                 Floor floorTile = Instantiate(floorPrefab, transform).GetComponent<Floor>();
+                float c = (Random.value + 9) / 10;
+                floorTile.GetComponent<SpriteRenderer>().color = new Color(c, c, c);
                 floorTile.transform.position = pos;
                 floor.Add(floorTile);
-                
             }
         }
+
+        
+     
+
+
+       
+    }
+
+    public Robot SpawnRobot()
+    {
+        Robot robot = Instantiate(robotPrefab).GetComponent<Robot>();
+        robots.Add(robot);
+        return robot;
+    }
+
+    public Vector3 CheckBounds(Vector2 pos) {
+        Vector2 newPos = pos;
+        if (pos.x > gridSize.x / 2) {
+            newPos.x = -gridSize.x / 2;
+        }
+        if (pos.x < -gridSize.x / 2)
+        {
+            newPos.x = gridSize.x / 2;
+        }
+        if (pos.y > gridSize.x / 2)
+        {
+            newPos.y = -gridSize.y / 2;
+        }
+        if (pos.y < -gridSize.y / 2)
+        {
+            newPos.y = gridSize.y / 2;
+        }
+        return newPos;
+    }
+    public Vector3 SnapToGrid(Vector3 pos) {
+        return new Vector3((int)pos.x, (int)pos.y, 0);
     }
 
 
@@ -39,6 +85,15 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (time > timeStep)
+        {
+            time = 0;
+            for (int i = 0; i < robots.Count; i++)
+            {
+                Robot robot = robots[i];
+                robot.RunProgram();
+            }
+        }
+        time += Time.deltaTime;
     }
 }
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
+    Vector3 direction = Vector2.zero;
+
+    static Vector3 north = new Vector3(0, 1, 0);
+    static Vector3 east = new Vector3(1, 0, 0);
+    static Vector3 south = new Vector3(0, -1, 0);
+    static Vector3 west = new Vector3(-1, 0, 0);
+    static Vector3[] directions = new Vector3[] {north,east,south,west};
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = RandomDirection();
+    }
+
+    public Vector2 RandomDirection() {
+        return directions[(int)(Random.value*(directions.Length-0.1f))];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RunProgram() {
+        direction = RandomDirection();
+        transform.position += direction;
     }
 }
